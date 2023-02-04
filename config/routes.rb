31db8c94fcde2b3919883babf026e6b7d0867245
good1luck21 <good1luck21@gmail.com>
2023-02-04 16:34:46 +0900

    lesson 2023-02-11

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post 'posts' => 'posts#create', as: 'posts_create'
   get 'posts/:kang/edit' => 'posts#edit',as: 'posts_edit'
   patch 'posts/:sato/edit'=> 'posts#update',as:'posts_update'
- 
+  get 'signup' => 'users#new', as: 'signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
