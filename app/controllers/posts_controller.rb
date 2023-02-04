cd5197117a778c8be1be107f80e2df86e4851c78
EC2 Default User <ec2-user@ip-172-31-1-184.ap-northeast-1.compute.internal>
2023-02-04 06:03:45 +0000

    lesson 2023-02-04

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,13 @@ class PostsController < ApplicationController
   end
   
   def edit
+    # params[:kang]
+    p "======edit======="
+    p params[:kang] # no problem
+    @post=Post.find_by(id: params[:kang]) # ここが問題かも
+    p "=======edit @post========="
+    p @post
+    # edit action no problem
   end
   
   def create
@@ -15,4 +22,26 @@ class PostsController < ApplicationController
     @post.save
     redirect_to root_path
   end
+  def update
+      @post=Post.find_by(id: params[:sato]) # params[:sato] -> :sato routes.rb patch 'posts/:sato/edit'
+      p "==========params check============"
+    p params
+     p "======================"
+     # update
+     # 必要な値 contentの内容を更新したい
+     # params[:sato] routes.rb
+     # params[:sato][:kang]
+      p "==========params check============"
+      p "urlのIDは#{params[:sato]}"
+      p "画面から入力された値は#{params[:kang]}"
+      p "======================"
+     
+      @post.update(content: params[:kang])
+      redirect_to  root_path
+   # p "params update start"
+   # p params
+   # p "params update end"
+  
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
   post 'posts' => 'posts#create', as: 'posts_create'
-  get 'edit' => 'posts#edit'
+  get 'posts/:kang/edit' => 'posts#edit',as: 'posts_edit'
+  patch 'posts/:sato/edit'=> 'posts#update',as:'posts_update'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
